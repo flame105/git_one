182cc0d119c5db0b5b0f2721de723fe5ccd7d1d3
zhouguoqiang <zhouguoqiang@tcl.com>
2017-08-18 17:48:57 +0800

    the frist commit

---
+++ yace.sh
@@ -0,0 +1,21 @@
+
+echo "............"
+
+set i=0   
+set j=0
+while true
+do              
+let "j=j+1 "      
+echo "j is $j "        
+
+input keyevent 23
+      sleep 5
+input keyevent 22 
+        sleep 2
+input keyevent 23
+      sleep 1200
+input keyevent 4
+        sleep 5
+input keyevent 23
+      sleep 5
+done 
